b4d44443043d6752e83b6c7dda37c91598fa3535
Dez Calimese <dezcalimese@gmail.com>
2024-08-08 15:17:54 -0400

    added light/dark mode

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { FaXTwitter } from "react-icons/fa6"; // Import the X (Twitter) icon
+import { ModeToggle } from "@/components/ui/toggle-mode";
 
 const Header: React.FC = () => {
   return (
     <header className="flex flex-col space-y-8 lg:flex-row justify-between items-center p-8">
-      <div className="text-7xl font-bold">Quincy Labs</div>
-      <Button variant="outline" className="px-6 py-5 text-2xl">
-        <Link
-          href="https://x.com/Quincy_Labs"
-          target="_blank"
-          className="flex items-center"
-        >
-          Follow on <FaXTwitter className="ml-2 mt-0.5 w-5 h-5" />
-        </Link>
-      </Button>
+      <div className="text-5xl font-bold whitespace-nowrap">Quincy Labs</div>
+      <div className="flex space-x-4">
+        {" "}
+        <Button variant="outline" className="px-6 py-[1.2rem] text-2xl">
+          <Link
+            href="https://x.com/Quincy_Labs"
+            target="_blank"
+            className="flex items-center"
+          >
+            Follow on <FaXTwitter className="ml-2 mt-0.5 w-5 h-5" />
+          </Link>
+        </Button>
+        <ModeToggle />
+      </div>
     </header>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Quicksand } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import Header from "./_components/Header";
 
@@ -19,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <Header />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
