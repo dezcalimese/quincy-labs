f25b83f6257f51eeafe102bf4769d43230831cc8
Dez Calimese <dezcalimese@gmail.com>
2024-08-31 19:41:55 -0400

    updated font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import "./fonts.css";
 import Header from "./_components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Quincy Labs",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="font-departure-mono">
         <ThemeProvider attribute="class" defaultTheme="system">
           <Header />
           {children}
